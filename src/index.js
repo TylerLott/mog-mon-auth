f8fae721a51ad4bda4acd5c64e0fa1aa78ac68c3
TylerLott <tylerlott0126@gmail.com>
2022-04-17 03:32:55 -0600

    ready for deploy

--- src/index.js
+++ src/index.js
@@ -3,14 +3,30 @@ import cors from "cors"
 import mongoose from "mongoose"
 
 // CONSTANTS
-const PORT = 5000
+let PORT = 80
+let MONGO_PATH = "mongodb://172.31.91.101:27017/monday"
+let PEER_HOST = "ludwigmonday.gg"
+let PEER_PATH = "/api/gangbang"
+let PEER_PORT = 80
+let WEBSOCKET_PATH = "http://ludwigmonday.gg/api/stepbrother"
+let ENDPOINT = "/api/spermbank"
+
+if (process.env.NODE_ENV !== "production") {
+  PORT = 5000
+  MONGO_PATH = "mongodb://localhost:27017/codesdb"
+  PEER_HOST = "localhost"
+  PEER_PATH = "/peer"
+  PEER_PORT = 7000
+  WEBSOCKET_PATH = "http://localhost:4000"
+  ENDPOINT = "/api/auth"
+}
 
 // SERVER
 const app = express()
 app.use(cors())
 
 // DATABASE
-mongoose.connect("mongodb://localhost:27017/codesdb", {
+mongoose.connect(MONGO_PATH, {
   useNewUrlParser: true,
 })
 const db = mongoose.connection
@@ -20,31 +36,23 @@ db.once("open", () => {
 })
 
 // DB MODEL
-const Room = mongoose.model(
-  "Room",
-  {
-    name: { type: String },
-    host: { type: String },
-    player: { type: String },
-    peerHost: { type: String },
-    peerPath: { type: String },
-    peerPort: { type: String },
-    socketURL: { type: String },
-  },
-  "codes"
-)
+const Room = mongoose.model("room", {
+  name: { type: String },
+  host: { type: String },
+  player: { type: String },
+})
 
-const Admin = mongoose.model(
-  "Admin",
-  {
-    username: { type: String },
-    password: { type: String },
-  },
-  "admin"
-)
+// const Admin = mongoose.model(
+//   "Admin",
+//   {
+//     username: { type: String },
+//     password: { type: String },
+//   },
+//   "admin"
+// )
 
 // ENDPOINTS
-app.get("/api/auth", (req, res) => {
+app.get(ENDPOINT, (req, res) => {
   if (req.query.roomcode && req.query.userId) {
     Room.findOne({ player: req.query.roomcode })
       .then((data) => {
@@ -54,10 +62,10 @@ app.get("/api/auth", (req, res) => {
               roomcode: data.name,
               userId: req.query.userId,
               type: "player",
-              peerHost: data.peerHost,
-              peerPath: data.peerPath,
-              peerPort: data.peerPort,
-              socketURL: data.socketURL,
+              peerHost: PEER_HOST,
+              peerPath: PEER_PATH,
+              peerPort: PEER_PORT,
+              socketURL: WEBSOCKET_PATH,
             })
           )
         } else {
@@ -69,10 +77,10 @@ app.get("/api/auth", (req, res) => {
                     roomcode: data.name,
                     userId: req.query.userId,
                     type: "host",
-                    peerHost: data.peerHost,
-                    peerPath: data.peerPath,
-                    peerPort: data.peerPort,
-                    socketURL: data.socketURL,
+                    peerHost: PEER_HOST,
+                    peerPath: PEER_PATH,
+                    peerPort: PEER_PORT,
+                    socketURL: WEBSOCKET_PATH,
                   })
                 )
               }
@@ -84,100 +92,100 @@ app.get("/api/auth", (req, res) => {
   }
 })
 
-app.get("/api/admin", (req, res) => {
-  // return all rooms
-  if (req.body.content.username && req.body.content.password) {
-    Admin.findOne({
-      username: req.body.content.username,
-      password: req.body.content.password,
-    })
-      .then((data) => {
-        if (!data) {
-          return res.status(401).send("string")
-        }
-      })
-      .catch((err) => res.status(401).send("string"))
-  } else {
-    return res.status(401).send("string")
-  }
+// app.get("/api/admin", (req, res) => {
+//   // return all rooms
+//   if (req.body.content.username && req.body.content.password) {
+//     Admin.findOne({
+//       username: req.body.content.username,
+//       password: req.body.content.password,
+//     })
+//       .then((data) => {
+//         if (!data) {
+//           return res.status(401).send("string")
+//         }
+//       })
+//       .catch((err) => res.status(401).send("string"))
+//   } else {
+//     return res.status(401).send("string")
+//   }
 
-  Room.find()
-    .then((rooms) => res.send(rooms))
-    .catch((err) =>
-      res.status(500).send({ message: err.message || "Error getting rooms" })
-    )
-})
+//   Room.find()
+//     .then((rooms) => res.send(rooms))
+//     .catch((err) =>
+//       res.status(500).send({ message: err.message || "Error getting rooms" })
+//     )
+// })
 
-app.post("/api/admin", (req, res) => {
-  // handle creating rooms
-  if (!req.body.content) {
-    return res.status(400).send({
-      message: "content cannot be empty",
-    })
-  }
+// app.post("/api/admin", (req, res) => {
+//   // handle creating rooms
+//   if (!req.body.content) {
+//     return res.status(400).send({
+//       message: "content cannot be empty",
+//     })
+//   }
 
-  if (req.body.content.username && req.body.content.password) {
-    Admin.findOne({
-      username: req.body.content.username,
-      password: req.body.content.password,
-    })
-      .then((data) => {
-        if (!data) {
-          return res.status(401).send("string")
-        }
-      })
-      .catch((err) => res.status(401).send("string"))
-  } else {
-    return res.status(401).send("string")
-  }
+//   if (req.body.content.username && req.body.content.password) {
+//     Admin.findOne({
+//       username: req.body.content.username,
+//       password: req.body.content.password,
+//     })
+//       .then((data) => {
+//         if (!data) {
+//           return res.status(401).send("string")
+//         }
+//       })
+//       .catch((err) => res.status(401).send("string"))
+//   } else {
+//     return res.status(401).send("string")
+//   }
 
-  const room = new Room({
-    name: req.body.content.name,
-    host: req.body.content.host,
-    player: req.body.content.player,
-  })
-  room
-    .save()
-    .then((data) => {
-      res.send(data)
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .send({ message: err.message || "error while creating room" })
-    })
-})
+//   const room = new Room({
+//     name: req.body.content.name,
+//     host: req.body.content.host,
+//     player: req.body.content.player,
+//   })
+//   room
+//     .save()
+//     .then((data) => {
+//       res.send(data)
+//     })
+//     .catch((err) => {
+//       res
+//         .status(500)
+//         .send({ message: err.message || "error while creating room" })
+//     })
+// })
 
-app.delete("api/admin", (req, res) => {
-  // handle deleting rooms
-  if (req.body.content.username && req.body.content.password) {
-    Admin.findOne({
-      username: req.body.content.username,
-      password: req.body.content.password,
-    })
-      .then((data) => {
-        if (!data) {
-          return res.status(401).send("string")
-        }
-      })
-      .catch((err) => res.status(401).send("string"))
-  } else {
-    return res.status(401).send("string")
-  }
+// app.delete("api/admin", (req, res) => {
+//   // handle deleting rooms
+//   if (req.body.content.username && req.body.content.password) {
+//     Admin.findOne({
+//       username: req.body.content.username,
+//       password: req.body.content.password,
+//     })
+//       .then((data) => {
+//         if (!data) {
+//           return res.status(401).send("string")
+//         }
+//       })
+//       .catch((err) => res.status(401).send("string"))
+//   } else {
+//     return res.status(401).send("string")
+//   }
 
-  Room.findOne({ name: req.body.content.name })
-    .then((room) => {
-      if (!room) {
-        return res.status(404).send({ message: "Room not found" })
-      }
-    })
-    .catch((err) => {
-      if (err.kind === "ObjectId" || err.name === "NotFound") {
-        return res.status(404).send({ message: "Room not found" })
-      }
-      return res.status(500).send({ message: "Could not delete room" })
-    })
-})
+//   Room.findOne({ name: req.body.content.name })
+//     .then((room) => {
+//       if (!room) {
+//         return res.status(404).send({ message: "Room not found" })
+//       }
+//     })
+//     .catch((err) => {
+//       if (err.kind === "ObjectId" || err.name === "NotFound") {
+//         return res.status(404).send({ message: "Room not found" })
+//       }
+//       return res.status(500).send({ message: "Could not delete room" })
+//     })
+// })
 
 // SERVER LISTEN
 app.listen(PORT, () => {
